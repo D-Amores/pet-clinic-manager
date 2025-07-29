b075cbbcc73100dec008bb3c6bc89da3ab643d60
Amores <carlos.amores19@outlook.com>
2025-07-29 17:58:24 -0600

    User authentication routing was created

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,59 @@
+ import {Link} from 'react-router-dom';
+ 
  const Login = () => {
   return (
     <>
-        <h2>Login</h2>
+        <div>
+          <h1 className="text-indigo-600 font-black text-6xl">
+            Inicia Sesion y Administra tus {""}
+            <span className="text-black">Pacientes</span>
+          </h1>
+        </div>
+
+        <div className='mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white'>
+          <form>
+            <div className="my-5">
+              <label
+                className="uppercase text-gray-600 block text-xl font-bold"
+              >
+                Email
+              </label>
+              <input 
+                type="email"
+                placeholder="Ingresa tu Email" 
+                className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+              />
+            </div>
+
+            <div className="my-5">
+              <label
+                className="uppercase text-gray-600 block text-xl font-bold"
+              >
+                Contraseña
+              </label>
+              <input 
+                type="password"
+                placeholder="Ingresa tu contraseña" 
+                className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+              />
+            </div>
+
+            <input 
+              type="submit" 
+              value="Iniciar Sesion"
+              className="bg-indigo-700 w-full py-3 px-10 rounded-xl text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800 md:w-auto"
+            />
+          </form>
+
+          <nav className='mt-10 lg:flex lg:justify-between'>
+            <Link
+              className='block text-center my-5 text-gray-500'
+              to="/register">¿No tienes cuenta? - registrate</Link>
+            <Link
+              className='block text-center my-5 text-gray-500' 
+              to="/forget-password">Olvide mi contraseña</Link>
+          </nav>
+        </div>
     </>
   )
 }
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,85 @@
+import { Link } from "react-router-dom"
+
 const Register = () => {
   return (
     <>
-        <h1>Desde REgister</h1>
+      <div>
+        <h1 className="text-indigo-600 font-black text-6xl">
+          Crea tu cuenta y administra {""}
+          <span className="text-black">tus Pacientes</span>
+        </h1>
+      </div>
+
+      <div className='mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white'>
+          <form>
+            <div className="my-5">
+              <label
+                className="uppercase text-gray-600 block text-xl font-bold"
+              >
+                Nombre
+              </label>
+              <input 
+                type="text"
+                placeholder="Ingresa tu Nombre" 
+                className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+              />
+            </div>
+
+            <div className="my-5">
+              <label
+                className="uppercase text-gray-600 block text-xl font-bold"
+              >
+                Email
+              </label>
+              <input 
+                type="email"
+                placeholder="Ingresa tu Email" 
+                className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+              />
+            </div>
+
+            <div className="my-5">
+              <label
+                className="uppercase text-gray-600 block text-xl font-bold"
+              >
+                Constraseña
+              </label>
+              <input 
+                type="password"
+                placeholder="Ingresa tu Constraseña" 
+                className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+              />
+            </div>
+
+            <div className="my-5">
+              <label
+                className="uppercase text-gray-600 block text-xl font-bold"
+              >
+                Repite tu Contraseña
+              </label>
+              <input 
+                type="email"
+                placeholder="Ingresa de nuevo tu contraseña" 
+                className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+              />
+            </div>
+
+            <input 
+              type="submit" 
+              value="Crear Cuenta"
+              className="bg-indigo-700 w-full py-3 px-10 rounded-xl text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800 md:w-auto"
+            />
+          </form>
+
+          <nav className='mt-10 lg:flex lg:justify-between'>
+            <Link
+              className='block text-center my-5 text-gray-500'
+              to="/">¿Ya tienes una cuenta? - Inicia Sesion</Link>
+            <Link
+              className='block text-center my-5 text-gray-500' 
+              to="/forget-password">Olvide mi contraseña</Link>
+          </nav>
+      </div>
     </>
   )
 }
