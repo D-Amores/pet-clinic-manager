aeeb6b088cddaf0128de043e4c3b6b9e6b60e26c
amores <amores.dev@outlook.com>
2025-08-15 22:34:32 -0600

    feat: add protected routes header layout

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,19 @@
-
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <h1>HEADER</h1>
+    <header className="py-10 bg-indigo-600">
+        <div className="container mx-auto flex flex-col lg:flex-row justify-between items-center">
+            <h1 className="font-bold text-2xl text-indigo-200 text-center">Administrador de {''} <span className="text-white font-black">Veterinaia</span></h1>
+
+            <nav className="flex flex-col items-center lg:flex-row gap-4 mt-5 lg:mt-0">
+                <Link to="/admin" className="text-white text-sm uppercase font-bold">Pacientes</Link>
+                <Link to="/admin" className="text-white text-sm uppercase font-bold">Perfil</Link>
+
+                <button className="text-white text-sm uppercase font-bold ">Cerrar Sesion</button>
+            </nav>
+        </div>
+    </header>
   )
 }
 
