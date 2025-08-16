eb18483a9988698ce4b436ff8f87ca925e71bdc6
amores <amores.dev@outlook.com>
2025-08-16 11:51:23 -0600

    feat: add toggle button for patient form

--- frontend/src/pages/ManagePatient.jsx
+++ frontend/src/pages/ManagePatient.jsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
 import Form from "../components/Form";
 import PatientList from "../components/PatientList";
 
 const ManagePatient = () => {
+  const [showForm, setShowForm] = useState(false);
+
   return (
     <div className="flex flex-col md:flex-row">
-      <div className="md:w-1/2 lg:w-2/5">
+      <button
+        type="button"
+        className="bg-indigo-600 text-white font-bold uppercase mx-10 p-3 rounded-md mb-10 md:hidden"
+        onClick={() => setShowForm(!showForm)}
+      >{showForm ? 'Ocultar Formulario' : 'Mostrar Formulario'}</button>
+
+      <div className={`${showForm ? 'block' : 'hidden' } md:block md:w-1/2 lg:w-2/5`}>
         <Form/>
       </div>
 
